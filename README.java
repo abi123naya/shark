beda1d16c485529e7e194e35bc77873f28a11474
abi123naya <connect2abinaya@gmail.com>
2016-07-08 10:46:19 +0530

    Update and rename README.md to README.java
    finish

---
+++ README.java
@@ -0,0 +1,16 @@
+# shark
+import.java.util.*;
+class OddEven 
+{ public static void main(String[] args)
+  { int x;
+  Scanner s=new Scanner(System.in);
+  x=s.nextInt();
+  }
+void dec()
+{ if(x%2==0)
+  {System.out.println("Even");}
+}
+else { System.out.println("Odd");}
+
+
+}
